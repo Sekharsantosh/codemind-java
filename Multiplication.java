9789908a02eaa01b122530a3483cfa03cb0b7b66
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-09-11 11:37:48 +0530

    import java.util.*; class multiply {     public static void main(String[] args)     {       Scanner sc=new Scanner(System.in);       int a,b;       a=sc.nextInt();       b=sc.nextInt();       int p=a*b;       System. out.println(p);     } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class multiply
+{
+    public static void main(String[] args)
+    {
+      Scanner sc=new Scanner(System.in);
+      int a,b;
+      a=sc.nextInt();
+      b=sc.nextInt();
+      int p=a*b;
+      System. out.println(p);
+    }
+}
