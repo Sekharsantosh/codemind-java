e582f1cd93afd39314b78ed4b24b47d95505cacf
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-10-04 10:37:37 +0530

    import java.io.BufferedReader; import java.io.InputStreamReader; import java.util.Scanner; class Multiply {    public static void main(String[] args) {        Scanner s=new Scanner(System.in);        float n=s.nextFloat();        float k=s.nextFloat();        System.out.format("%.2f",n*k);     }
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+class Multiply {
+   public static void main(String[] args) {
+       Scanner s=new Scanner(System.in);
+       float n=s.nextFloat();
+       float k=s.nextFloat();
+       System.out.format("%.2f",n*k);
+    }
+  
+}
