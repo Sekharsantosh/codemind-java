e0357231942ea782e1909e621b9aa578a0c62bca
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-10-06 10:00:57 +0530

    import java.util.Scanner; class input{     public static void main(String args[]){         String k;         Scanner s=new Scanner(System.in);         k=s.nextLine();         System.out.println(k);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class input{
+    public static void main(String args[]){
+        String k;
+        Scanner s=new Scanner(System.in);
+        k=s.nextLine();
+        System.out.println(k);
+    }
+}
