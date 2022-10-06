6e862dd95c0599a79194e74af1fb3db0e14f3d71
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2022-10-06 09:47:06 +0530

    import java.util.Scanner; class area{     public static void main(String args[]){         int n,m,f;         Scanner s=new Scanner(System.in);         n=s.nextInt();         m=s.nextInt();         f=n/m;         System.out.println(f);
        }
    }

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class area{
+    public static void main(String args[]){
+        int n,m,f;
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        m=s.nextInt();
+        f=n/m;
+        System.out.println(f);
+        
+        
+    }
+}
