f6568eb0be565d47aa5d123dd65c5e00eca2b910
Santosh <97425696+Sekharsantosh@users.noreply.github.com>
2023-02-02 06:35:11 +0530

    import java.util.*; public class file{     public static void main(String[] args){         String n; 		Scanner sc=new Scanner(System.in); 		n=sc.next(); 		switch(n){ 		    case "A": System.out.println("Vowel"); 		              break; 		    case "E": System.out.println("Vowel"); 		              break; 		    case "I":  System.out.println("Vowel"); 		               break; 		    case "O": System.out.println("Vowel"); 		              break; 		    case "U": System.out.println("Vowel"); 		              break; 		    case "a": System.out.println("Vowel"); 		              break; 		    case "e": System.out.println("Vowel"); 		              break; 		    case "i":System.out.println("Vowel"); 		             break; 		    case "o": System.out.println("Vowel"); 		              break; 		    case "u": System.out.println("Vowel"); 		              break; 		   default :System.out.println("Consonant"); 		}
        }
    }

---
+++ Program_to_check_Vowel_or_Consonant.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class file{
+    public static void main(String[] args){
+        String n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.next();
+		switch(n){
+		    case "A": System.out.println("Vowel");
+		              break;
+		    case "E": System.out.println("Vowel");
+		              break;
+		    case "I":  System.out.println("Vowel");
+		               break;
+		    case "O": System.out.println("Vowel");
+		              break;
+		    case "U": System.out.println("Vowel");
+		              break;
+		    case "a": System.out.println("Vowel");
+		              break;
+		    case "e": System.out.println("Vowel");
+		              break;
+		    case "i":System.out.println("Vowel");
+		             break;
+		    case "o": System.out.println("Vowel");
+		              break;
+		    case "u": System.out.println("Vowel");
+		              break;
+		   default :System.out.println("Consonant");
+		}
+        
+    }
+}
